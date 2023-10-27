81c55020751c25b44d8c158c6869f03d040bec78
Josh Thornton <josh@speedscale.com>
2023-10-27 12:51:36 -0400

    backup

--- bkp.sh
+++ bkp.sh
@@ -8,15 +8,18 @@ echo 'copying vim configs...'
 cp ~/.vimrc ./vim/vimrc
 cp -r ~/.config/nvim/ ./vim
 
-echo 'copying karabiner.json...'
-cp ~/.config/karabiner/karabiner.json ./karabiner.json
-
 echo 'copying zshrc...'
-grep '### personal ###' ~/.zshrc -A 10000 > zshrc
+cp ~/.zshrc zshrc
 
 echo 'copying gitconfig...'
 cp ~/.gitconfig ./git/gitconfig
 cp -r ~/.config/git/ ./git
+
+echo 'copying karabiner.json...'
+cp ~/.config/karabiner/karabiner.json ./karabiner.json
+
+echo 'copying k9s config...'
+cp -r ~/.config/k9s/ ./k9s
 
 echo '--- done ---'
 echo
