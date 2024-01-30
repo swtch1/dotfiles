d6cd3de8adcc83bfceffaa4da0a726f069debdad
Josh Thornton <josh@speedscale.com>
2024-01-30 15:34:31 -0500

    backup

--- vim/lua/plug.lua
+++ vim/lua/plug.lua
@@ -125,3 +125,35 @@ require('bqf').setup{
   }
 }
 
+require('gitsigns').setup {
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Actions
+    map('n', '<leader>cc', gs.next_hunk)
+    map('n', '<leader>cC', gs.prev_hunk)
+    map('n', '<leader>cs', gs.stage_hunk)
+    map('n', '<leader>cu', gs.reset_hunk)
+    map('v', '<leader>cs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('v', '<leader>cu', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    -- map('n', '<leader>hS', gs.stage_buffer)
+    -- map('n', '<leader>hu', gs.undo_stage_hunk)
+    -- map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>cd', gs.preview_hunk)
+    -- map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    -- map('n', '<leader>tb', gs.toggle_current_line_blame)
+    -- map('n', '<leader>hd', gs.diffthis)
+    -- map('n', '<leader>hD', function() gs.diffthis('~') end)
+    -- map('n', '<leader>td', gs.toggle_deleted)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
+
