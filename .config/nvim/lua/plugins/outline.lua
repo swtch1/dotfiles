2be2c819ed6038ab0b75c703afaba46473668ade
Josh Thornton <josh@speedscale.com>
2024-08-13 10:09:55 -0400

    backup

--- .config/nvim/lua/plugins/outline.lua
+++ .config/nvim/lua/plugins/outline.lua
@@ -1,8 +1,15 @@
 return {
-  'hedyhli/outline.nvim',
+  "hedyhli/outline.nvim",
   lazy = true,
-  cmd = "Outline",
-  config = function()
-    require("outline").setup()
-  end
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<leader>fo", "<cmd>Outline<CR>", desc = "Outline" },
+  },
+  opts = {
+    outline_window = {
+      position = "left",
+      auto_close = true,
+      show_relative_numbers = true,
+    },
+  },
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,7 @@
 return {
-  "nvim-telescope/telescope.nvim", opts = {}
+  "nvim-telescope/telescope.nvim",
+  init = function()
+    vim.keymap.set('n', '<leader>p', '<cmd>Telescope find_files<cr>')
+    vim.keymap.set('n', '<leader>p', '<cmd>Telescope find_files %<cr>')
+  end
 }
