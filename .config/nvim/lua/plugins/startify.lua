519c09a5c3e21b802770ebc208182eddd354d73a
Josh Thornton <josh@speedscale.com>
2024-08-13 10:32:19 -0400

    backup

--- .config/nvim/lua/plugins/startify.lua
+++ .config/nvim/lua/plugins/startify.lua
@@ -1,3 +1,19 @@
 return {
-  'mhinz/vim-startify'
+  'mhinz/vim-startify',
+  init = function()
+    vim.g.startify_change_to_dir = 0
+    vim.g.startify_bookmarks = {
+      { c = '~/.speedscale/config.yaml' },
+      { v = '~/.vimrc' },
+      { z = '~/.zshrc' }
+    }
+    vim.g.startify_enable_special = 0
+    vim.g.startify_lists = {
+      { type = 'dir', header = { '   MRU ' .. vim.fn.getcwd() } },
+      { type = 'files', header = { '   MRU' } },
+      { type = 'sessions', header = { '   Sessions' } },
+      { type = 'bookmarks', header = { '   Bookmarks' } },
+      { type = 'commands', header = { '   Commands' } }
+    }
+  end
 }
