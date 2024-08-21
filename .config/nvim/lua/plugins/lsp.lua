d99c8257c8a3bd3b821c3a44245d8fde9d432143
Josh Thornton <josh@speedscale.com>
2024-08-21 10:44:31 -0400

    backup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,71 +1,81 @@
 return {
-  {
-    'williamboman/mason.nvim',
-    opts = {
-      ui = {
-	icons = {
-	  package_installed = '✓',
-	  package_pending = '➜',
-	  package_uninstalled = '✗',
+	{
+		'williamboman/mason.nvim',
+		opts = {
+			ui = {
+				icons = {
+					package_installed = '✓',
+					package_pending = '➜',
+					package_uninstalled = '✗',
+				},
+			},
+		},
 	},
-      },
-    },
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = {
-      'williamboman/mason.nvim',
-    },
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      'SmiteshP/nvim-navic',
-      'williamboman/mason.nvim',
-    },
-  },
-  { 'hrsh7th/nvim-cmp', },
-  { 'hrsh7th/cmp-nvim-lsp', },
-  { 'hrsh7th/cmp-buffer', },
-  { 'hrsh7th/cmp-path', },
-  {
-    'hrsh7th/cmp-nvim-lua',
-    lazy = true,
-    ft = { 'lua' },
-  },
-  { 'mfussenegger/nvim-jdtls' },
-  {
-    'SmiteshP/nvim-navic',
-    opts = {
-      icons = {
-        File          = "",
-        Module        = "",
-        Namespace     = "",
-        Package       = "",
-        Class         = "",
-        Method        = "",
-        Property      = "",
-        Field         = "",
-        Constructor   = "",
-        Enum          = "",
-        Interface     = "",
-        Function      = "",
-        Variable      = "",
-        Constant      = "",
-        String        = "",
-        Number        = "",
-        Boolean       = "",
-        Array         = "",
-        Object        = "",
-        Key           = "",
-        Null          = "",
-        EnumMember    = "",
-        Struct        = "",
-        Event         = "",
-        Operator      = "",
-        TypeParameter = "",
-      },
-      separator = ' > ',
-    },
-  },
+	{
+		'williamboman/mason-lspconfig.nvim',
+		dependencies = {
+			'williamboman/mason.nvim',
+		},
+	},
+	{
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			'SmiteshP/nvim-navic',
+			'williamboman/mason.nvim',
+		},
+		init = function()
+			-- auto format on save
+			vim.api.nvim_create_augroup("AutoFormat", {})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = "AutoFormat",
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	},
+	{ 'hrsh7th/nvim-cmp', },
+	{ 'hrsh7th/cmp-nvim-lsp', },
+	{ 'hrsh7th/cmp-buffer', },
+	{ 'hrsh7th/cmp-path', },
+	{
+		'hrsh7th/cmp-nvim-lua',
+		lazy = true,
+		ft = { 'lua' },
+	},
+	{ 'mfussenegger/nvim-jdtls' },
+	{
+		'SmiteshP/nvim-navic',
+		opts = {
+			icons = {
+				File          = "",
+				Module        = "",
+				Namespace     = "",
+				Package       = "",
+				Class         = "",
+				Method        = "",
+				Property      = "",
+				Field         = "",
+				Constructor   = "",
+				Enum          = "",
+				Interface     = "",
+				Function      = "",
+				Variable      = "",
+				Constant      = "",
+				String        = "",
+				Number        = "",
+				Boolean       = "",
+				Array         = "",
+				Object        = "",
+				Key           = "",
+				Null          = "",
+				EnumMember    = "",
+				Struct        = "",
+				Event         = "",
+				Operator      = "",
+				TypeParameter = "",
+			},
+			separator = ' > ',
+		},
+	},
 }
