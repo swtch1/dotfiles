9876d853173421d1395d0e6ec536c97dad45b880
Josh Thornton <josh@speedscale.com>
2024-08-28 12:14:07 -0400

    backup

--- .config/lua/plugins/curl.lua
+++ .config/lua/plugins/curl.lua
@@ -1,11 +1,24 @@
+-- return {
+-- 	"oysandvik94/curl.nvim",
+-- 	cmd = { "CurlOpen" },
+-- 	dependencies = {
+-- 		"nvim-lua/plenary.nvim",
+-- 	},
+-- 	keys = {
+-- 		{ "<leader>rc", "<cmd>CurlOpen<cr><cmd>tabclose<cr>", desc = "Run curl cmd" },
+-- 	},
+-- 	config = true,
+-- }
 return {
 	"oysandvik94/curl.nvim",
-	cmd = { "CurlOpen" },
+	branch = "open_in_split",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
-	keys = {
-		{ "<leader>rc", "<cmd>CurlOpen<cr><cmd>tabclose<cr>", desc = "Run curl cmd" },
-	},
-	config = true,
+	config = function()
+		local curl = require("curl")
+		curl.setup({
+			open_with = "split",
+		})
+	end
 }
--- .config/lua/plugins/telescope.lua
+++ .config/lua/plugins/telescope.lua
@@ -1,22 +1,47 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = '0.1.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'BurntSushi/ripgrep', -- optional
-    'sharkdp/fd',         -- optional
-  },
-  lazy = true,
-  keys = {
-    { '<leader>p', '<cmd>Telescope find_files<cr>', desc = "Find files anywhere" },
-    { '<leader>P', ':lua require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h") })<CR>', desc = "Find files in current dir" },
-    { '<leader>fa', '<cmd>Telescope live_grep<cr>', desc = "Live grep" },
-    { '<leader>fb', '<cmd>Telescope current_buffer_fuzzy_find<cr>', desc = "Live grep current buffer" },
-    { '<leader>fA', ':lua require("telescope.builtin").live_grep({ cwd = vim.fn.expand("%:p:h") })<CR>', desc = "Live grep in current dir" },
-    { '<leader>gu', ':lua require("telescope.builtin").lsp_implementations()<CR>', desc = "LSP implementations" },
-    { '<leader>gR', ':lua require("telescope.builtin").lsp_references()<CR>', desc = "LSP references" },
-    { '<leader>gT', ':lua require("telescope.builtin").lsp_incoming_calls()<CR>', desc = "LSP incoming calls" },
-    { '<leader>fF', ':lua require("telescope.builtin").grep_string({ search = "FIXME: (JMT)" })<CR>', desc = "Grep FIXME (JMT)" },
-    { '<leader>fB', ':lua require("telescope.builtin").grep_string({ search = "BOOKMARK:" })<CR>', desc = "Grep BOOKMARK:" },
-  },
+	"nvim-telescope/telescope.nvim",
+	branch = '0.1.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'BurntSushi/ripgrep', -- optional
+		'sharkdp/fd',       -- optional
+	},
+	lazy = true,
+	keys = {
+		{ '<leader>p',  '<cmd>Telescope find_files<cr>',                                                      desc = "Find files anywhere" },
+		{ '<leader>P',  ':lua require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h") })<CR>', desc = "Find files in current dir" },
+		{ '<leader>fa', '<cmd>Telescope live_grep<cr>',                                                       desc = "Live grep" },
+		{ '<leader>fb', '<cmd>Telescope current_buffer_fuzzy_find<cr>',                                       desc = "Live grep current buffer" },
+		{ '<leader>fA', ':lua require("telescope.builtin").live_grep({ cwd = vim.fn.expand("%:p:h") })<CR>',  desc = "Live grep in current dir" },
+		{ '<leader>gu', ':lua require("telescope.builtin").lsp_implementations()<CR>',                        desc = "LSP implementations" },
+		{ '<leader>gR', ':lua require("telescope.builtin").lsp_references()<CR>',                             desc = "LSP references" },
+		{ '<leader>gT', ':lua require("telescope.builtin").lsp_incoming_calls()<CR>',                         desc = "LSP incoming calls" },
+		{ '<leader>fF', ':lua require("telescope.builtin").grep_string({ search = "FIXME: (JMT)" })<CR>',     desc = "Grep FIXME (JMT)" },
+		{ '<leader>fB', ':lua require("telescope.builtin").grep_string({ search = "BOOKMARK:" })<CR>',        desc = "Grep BOOKMARK:" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
+
+		telescope.setup({
+			defaults = {
+				-- send some or all entries to quickfix list with <C-q>
+				mappings = {
+					i = {
+						["<C-q>"] = function(bufnr)
+							actions.smart_send_to_qflist(bufnr)
+							actions.open_qflist()
+						end,
+					},
+					n = {
+						["<C-q>"] = function(bufnr)
+							actions.smart_send_to_qflist(bufnr)
+							actions.open_qflist()
+						end,
+					},
+				},
+			},
+		})
+	end,
 }
