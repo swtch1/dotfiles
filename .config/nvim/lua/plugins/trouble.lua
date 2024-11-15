6690b82e9a26175adbec6511cf4d88de0bb0b03e
Josh Thornton <josh@speedscale.com>
2024-11-15 14:29:32 -0500

    backup

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -2,7 +2,7 @@ return {
 	"folke/trouble.nvim",
 	opts = {
 		focus = true,
-		auto_close = true,
+		auto_close = false,
 		modes = {
 			diagnostics = {
 				auto_jump = false,
@@ -11,6 +11,10 @@ return {
 			lsp_base = {
 				auto_jump = true,
 				warn_no_results = true,
+				follow = false,
+				params = {
+					include_current = true,
+				},
 			},
 			symbols = {
 				win = {
@@ -23,7 +27,7 @@ return {
 	keys = {
 		{ "<leader>a",  "<cmd>Trouble diagnostics open<cr>",              desc = "All diagnostics", },
 		{ "<leader>A",  "<cmd>Trouble diagnostics open filter.buf=0<cr>", desc = "Buffer diagnostics", },
-		{ "<leader>gr", "<cmd>Trouble lsp_references open<cr>",           desc = "LSP references", },
+		{ "<leader>gr", "<cmd>Trouble lsp<cr>",                           desc = "LSP", },
 		{ "<leader>fo", "<cmd>Trouble symbols focus<cr>",                 desc = "Outline", },
 	},
 }
