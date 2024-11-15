20da82b1c365a21b55ab4a31053cb2e2e613b99f
Josh Thornton <josh@speedscale.com>
2024-11-15 10:43:02 -0500

    backup

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -79,9 +79,17 @@ return {
 		opts = {
 			---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
 			provider = "openai",
-			auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+			openai = {
+				endpoint = "https://api.openai.com/v1",
+				model = "gpt-4o",
+				timeout = 60000, -- milliseconds
+				temperature = 0,
+				-- max_tokens = 4096,
+				["local"] = false,
+			},
+			-- auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
 			behaviour = {
-				auto_suggestions = false,        -- Experimental stage
+				auto_suggestions = false, -- Experimental stage
 				auto_set_highlight_group = true,
 				auto_set_keymaps = true,
 				auto_apply_diff_after_generation = false,
