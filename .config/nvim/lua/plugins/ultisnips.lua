efe610be1e64da08fb219cc31e5d1bdfed2f99b6
Josh Thornton <josh@speedscale.com>
2025-04-28 09:57:14 -0400

    backup

--- .config/nvim/lua/plugins/ultisnips.lua
+++ .config/nvim/lua/plugins/ultisnips.lua
@@ -1,10 +1,8 @@
 return {
 	{
 		'SirVer/ultisnips',
-		ft = { 'go' }, -- FIXME: this didn't work with python
 	},
 	{
 		'quangnguyen30192/cmp-nvim-ultisnips',
-		ft = { 'go' }, -- FIXME: this didn't work with python
 	},
 }
